7182c24842104734340c795c58112670b78ffc86
janussen <k@janussen.dk>
2017-09-08 00:37:22 +0200

    Implement toggle follow (new feature, modularized)

---
+++ GigHub/Scripts/app/controllers/followingsController.js
@@ -0,0 +1,33 @@
+﻿var FollowingsController = function (followingsService) {
+    var button;
+
+    var init = function (container) {
+        $(container).on("click", ".js-toggle-following", toggleFollowing);
+    };
+
+    var toggleFollowing = function (e) {
+        button = $(e.target);
+
+        var followeeId = button.attr("data-artist-id");
+
+        if (button.hasClass("btn-default")) {
+            followingsService.createFollowing(followeeId, done, fail);
+        }
+        else {
+            followingsService.deleteFollowing(followeeId, done, fail);
+        }
+    };
+
+    var done = function () {
+        var text = (button.text() === "Following") ? "Following" : "Following?";
+        button.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    };
+
+    var fail = function () {
+        alert("Something failed!");
+    };
+
+    return {
+        init: init
+    };
+}(FollowingService);
